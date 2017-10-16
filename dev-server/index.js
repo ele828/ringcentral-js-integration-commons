eb2327c579d42b07a9e811cfc27f10b38fa5d115
Eric Huang <ele828@gmail.com>
2017-10-16 00:30:15 -0500

    Add DI document and fix lint (#437)
    * fix linting issue
    
    * add readme
    
    * code refactoring
    
    * update README
    
    * update dev-server
    
    * update README
    
    * fix typo

--- dev-server/index.js
+++ dev-server/index.js
@@ -4,48 +4,24 @@ import ReactDOM from 'react-dom';
 import { connect, Provider } from 'react-redux';
 import JSONTree from 'react-json-tree';
 
-// import Phone from './Phone';
-import { ModuleFactory, Module } from '../src/lib/di';
+import { ModuleFactory } from '../src/lib/di';
 import RcPhone from '../src/modules/RcPhone';
-import RcModule from '../src/lib/RcModule';
 import config from './config';
 
-@Module()
-class Chars {
-  toUpperCase(str) {
-    return str.toUpperCase();
-  }
-}
-
 @ModuleFactory({
   providers: [
-    RcPhone,
-    { provide: 'Chars', useClass: Chars }
-  ]
-})
-class Utils extends RcModule {}
-
-@ModuleFactory({
-  providers: [
-    { provide: Utils, private: false },
-    { provide: 'Config', useValue: config, private: true },
-    { provide: 'TestFactory',
-      useFactory: ({ utils }) => utils.chars.toUpperCase('test_factory'),
-      deps: ['Utils']
-    }
+    { provide: 'Config', useValue: config, private: true }
   ]
 })
 class Phone extends RcPhone {}
-console.time('init time');
 const phone = Phone.create();
-console.timeEnd('init time');
 global.phone = phone;
-const baseStore = createStore(phone.reducer);
-phone.setStore(baseStore);
+const store = createStore(phone.reducer);
+phone.setStore(store);
 
-// baseStore.subscribe(() => {
-//   console.log(baseStore.getState());
-// });
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 const DemoView = connect(state => ({
   data: state,
@@ -54,16 +30,8 @@ const DemoView = connect(state => ({
   shouldExpandNode: (keyName, data, level) => level < 2,
 }))(JSONTree);
 
-// const phone = new Phone();
-// const store = createStore(phone.reducer);
-// store.subscribe(() => {
-//   console.log(store.getState());
-// });
-// global.phone = phone;
-// phone.setStore(store);
-
 ReactDOM.render((
-  <Provider store={baseStore}>
+  <Provider store={store}>
     <DemoView />
   </Provider>
 ), document.querySelector('#viewport'));
